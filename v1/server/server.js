c6117594bdb6c502eef973eaf03225d541fbee55
ivanfabriano <ivanfabriano99@mail.ugm.ac.id>
2022-07-21 16:49:26 +0700

    first commit

---
+++ v1/server/server.js
@@ -0,0 +1,21 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
